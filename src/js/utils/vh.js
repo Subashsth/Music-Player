02ecdc5e0dbaf073df54023bb393b3cef3d0bbe4
karun <karun_sth@hotmail.com>
2023-12-08 14:06:56 +0545

    deleted

--- src/js/utils/vh.js
+++
@@ -1,9 +0,0 @@
-(function (window, document) {
-  const variable = '--full-viewport';
-  function vh() {
-    document.documentElement.style.setProperty(variable, `${window.innerHeight}px`);
-  }
-  vh();
-  window.addEventListener('resize', vh);
-  window.addEventListener('orientationchange', vh);
-})(window, document);
