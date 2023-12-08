f7af5d4ef06ab3a18eafd40c199034da14cb8947
karun <karun_sth@hotmail.com>
2023-12-08 13:46:14 +0545

    add

---
+++ src/js/components/data.js
@@ -0,0 +1,28 @@
+export default function initData() {
+  const musicData = [
+    {
+      name: 'music1',
+      path: 'musics/song1.mp3',
+      artist: 'artist1',
+      img: 'images/img1.jpg',
+    },
+    {
+      name: 'music2',
+      path: 'musics/song2.mp3',
+      artist: 'artist2',
+      img: 'images/img2.jpg',
+    },
+    {
+      name: 'music3',
+      path: 'musics/song3.mp3',
+      artist: 'artist3',
+      img: 'images/img3.jpg',
+    },
+    {
+      name: 'music4',
+      path: 'musics/song4.mp3',
+      artist: 'artist4',
+      img: 'images/img4.jpg',
+    },
+  ];
+}
